8d56540c66739aa79a16c40946cfe86212f31fd8
轩张 <zx804463232@gmail.com>
2021-05-21 17:45:59 +0800

    finished project

--- IHymInstaHighlight/Page/Main/IHymMainVC.swift
+++ IHymInstaHighlight/Page/Main/IHymMainVC.swift
@@ -11,10 +11,44 @@ class IHymMainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
         
     }
     
     
 
 }
+
+extension IHymMainVC {
+    func setupView() {
+        let settingBtn = UIButton(type: .custom)
+        let storeBtn = UIButton(type: .custom)
+        
+        view.addSubview(settingBtn)
+        view.addSubview(storeBtn)
+        
+        settingBtn.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(8)
+            $0.left.equalTo(10)
+            $0.width.height.equalTo(44)
+        }
+        storeBtn.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(8)
+            $0.right.equalTo(-10)
+            $0.width.height.equalTo(44)
+        }
+        settingBtn.addTarget(self, action: #selector(settingBtnClick(sender:)), for: .touchUpInside)
+        settingBtn.addTarget(self, action: #selector(storeBtnClick(sender:)), for: .touchUpInside)
+    }
+    
+    @objc func settingBtnClick(sender: UIButton) {
+        
+    }
+    @objc func storeBtnClick(sender: UIButton) {
+        
+    }
+    
+    
+}
+
+
---
+++ Podfile
@@ -0,0 +1,41 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'IHymInstaHighlight' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for IHymInstaHighlight
+  pod 'Then'
+  pod 'DeviceKit'
+  pod 'SwifterSwift'
+  pod 'Defaults'
+  pod 'NoticeObserveKit'
+  pod 'Adjust'
+  pod 'GPUImage'
+  
+  #  pod 'JXPagingView/Paging'
+  #  pod 'JXSegmentedView'
+  
+  #  UI
+  pod 'SnapKit'
+  
+  #  Alert
+  pod 'Alertift'
+  pod 'ZKProgressHUD'
+  
+  #  Store
+  pod 'SwiftyStoreKit'
+  pod 'TPInAppReceipt'
+  
+  #  Debug
+  pod 'LookinServer', :configurations => ['Debug']
+  
+  
+  # login
+  pod 'FirebaseUI'
+  pod 'FirebaseUI/Auth'
+  pod 'FirebaseUI/Google'
+  pod 'FirebaseUI/OAuth'
+  pod 'Firebase/Messaging'
+end
